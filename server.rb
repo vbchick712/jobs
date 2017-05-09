bedddfe06fc3a43f822b15a8a505c0e0e58f816e
Shannan Muench <shannan.muench@gmail.com>
2017-05-09 17:12:45 -0400

    Updated code for job scraper

--- server.rb
+++ server.rb
@@ -1,1 +1,15 @@
 require 'sinatra'
+require 'httparty'
+require 'nokogiri'
+
+get '/' do
+  url = "https://miami.craigslist.org/search/mdc/jjj?query=#{params["job"]}"
+  response = HTTParty.get url
+
+  # response.body
+  dom = Nokogiri::HTML(response.body)
+
+  dom.css(".hdrlnk").map do | a |
+    a.content + "</br>"
+  end
+end
